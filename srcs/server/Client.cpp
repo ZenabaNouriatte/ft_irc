44b7da3038167ca2102f484dc9f024c52a0d752c
Chloe <42.cours@gmail.com>
2025-07-15 13:28:12 +0200

    debut_Serv

--- srcs/server/Client.cpp
+++ srcs/server/Client.cpp
@@ -15,3 +15,26 @@ std::string& Client::getBuffer()
 {
     return _buffer;
 }
+
+int Client::getFd(void) const
+{
+	std::cout << "getFd member function called" << std::endl;
+	return (_fd);
+}
+
+void Client::send_msg(const std::string& message)
+{
+    std::string full_message = message + "\r\n";
+
+    ssize_t sent = send(_fd, full_message.c_str(), full_message.size(), 0);
+
+    if (sent == -1)
+    {
+        std::cerr << "[ERROR] Echec de l'envoi du message au client fd " << _fd << std::endl;
+    }
+    else
+    {
+        std::cout << "[SEND] (" << sent << " octets) --> Client fd " << _fd
+                  << " : " << message << std::endl;
+    }
+}
